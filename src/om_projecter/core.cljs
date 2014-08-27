1dc472f62c6a9d7a1fd8af84ea0580f56e82e285
Teemu Frisk <teemu.frisk@gmail.com>
2014-08-27 16:31:55 +0300

    Add editable component

--- src/om_projecter/core.cljs
+++ src/om_projecter/core.cljs
@@ -39,6 +39,49 @@
     ]
 }))
 
+;============= editable fields ======
+
+(defn display [show]
+  (if show
+    #js {}
+    #js {:display "none"}))
+
+(defn handle-change [e data edit-key owner]
+  (om/transact! data edit-key (fn [_] (.. e -target -value))))
+
+(defn on-edit [id title])
+
+(defn end-edit [text owner cb]
+  (om/set-state! owner :editing false)
+  (when cb
+    (cb text)))
+
+(defn editable [data owner {:keys [edit-key on-edit] :as opts}]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:editing false})
+    om/IRenderState
+    (render-state [_ {:keys [editing]}]
+      (let [text (get data edit-key)]
+        (dom/li nil
+          (dom/span #js {:style (display (not editing))} text)
+          (dom/input
+            #js {:style (display editing)
+                 :value text
+                 :onChange #(handle-change % data edit-key owner)
+                 :onKeyPress #(when (== (.-keyCode %) 13)
+                                (end-edit text owner on-edit))
+                 :onBlur (fn [e]
+                           (when (om/get-state owner :editing)
+                             (end-edit text owner on-edit)))})
+          (dom/button
+            #js {:style (display (not editing))
+                 :onClick #(om/set-state! owner :editing true)}
+            "Edit"))))))
+
+;============= show app data ======
+
 (defn get-user-name [userid]
   (:name (first
     (filter #(= userid (:id %)) (:users @app-state)))))
@@ -105,7 +148,10 @@
     om/IRender
     (render [_]
       (dom/div nil
-        (dom/li nil (str (:name entry)))))))
+        ;(dom/li nil (str (:name entry)))
+        (om/build editable entry
+          {:opts {:edit-key :name}})
+        ))))
 
 (defn projects-view [app owner]
   (reify
